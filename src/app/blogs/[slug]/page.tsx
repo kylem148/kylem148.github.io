e4673a46752f8a8d87c8637a5b2eb7735ceafa8c
kylem148 <jazzerflash@gmail.com>
2025-10-25 19:02:54 -0700

    feat: fix github error

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,25 +1,18 @@
 import Link from "next/link";
 import { Blogs } from "@/constants/blogData";
 
-// finds what slugs to generate
-export async function generateStaticParams() {
-  return Blogs.map((b) => ({ slug: b.slug }));
+interface BlogPageProps {
+  params: { slug: string };
 }
 
-// gets the data depending on the slug
-function getCardDetail(slug: string) {
-  return Blogs.find((b) => b.slug === slug) || null;
-}
-
-const BlogPageDetails = ({ params }: { params: { slug: string } }) => {
-  const blogCard = getCardDetail(params.slug);
+export default async function BlogPageDetails({ params }: BlogPageProps) {
+  const blogCard = Blogs.find((b) => b.slug === params.slug);
   if (!blogCard) return null;
 
   return (
     <main>
       {/* The top half */}
-      <section className="relative w-screen h-[50vh] flex flex-col justify-center  overflow-hidden p-10 md:p-30">
-        
+      <section className="relative w-screen h-[50vh] flex flex-col justify-center overflow-hidden p-10 md:p-30">
         {/* Background image */}
         <img
           src={blogCard.image}
@@ -27,11 +20,11 @@ const BlogPageDetails = ({ params }: { params: { slug: string } }) => {
           className="absolute inset-0 h-full w-full object-cover object-center opacity-70"
         />
 
-        {/* dark overlay*/}
+        {/* dark overlay */}
         <div className="absolute inset-0 bg-black/40" />
 
         {/* Foreground text */}
-        <div className="relative z-10 ">
+        <div className="relative z-10">
           <h1 className="font-bold text-amber-50 opacity-90 text-5xl md:text-7xl">
             {blogCard.title}
           </h1>
@@ -40,10 +33,10 @@ const BlogPageDetails = ({ params }: { params: { slug: string } }) => {
           </p>
         </div>
       </section>
-      
+
       {/* The bottom half */}
       <section className="max-w-4xl p-10 flex flex-col h-[50vh]">
-        <p className="text-lg md:text-xl ">{blogCard.description}</p>
+        <p className="text-lg md:text-xl">{blogCard.description}</p>
         <Link
           href="/#blog"
           className="mt-20 text-[0.95rem] text-green-500 hover:text-green-400"
@@ -53,6 +46,8 @@ const BlogPageDetails = ({ params }: { params: { slug: string } }) => {
       </section>
     </main>
   );
-};
+}
 
-export default BlogPageDetails;
+export async function generateStaticParams() {
+  return Blogs.map((b) => ({ slug: b.slug }));
+}
